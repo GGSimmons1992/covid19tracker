4ea410048bd640b17f149bb1ce156d955672a9fb
Gary Simmons <gary.simmons@colorado.edu>
2021-09-25 13:56:44 -0500

    merge data sets

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+import plotly
+from urllib.request import urlopen
+import json
+import plotly.express as px
+import plotly.graph_objects as go
