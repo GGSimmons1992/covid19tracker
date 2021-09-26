b18b5ceed95f1fb0d6eb091a8cf2620948f5fee3
Gary Simmons <gary.simmons@colorado.edu>
2021-09-26 12:59:44 -0500

    create choropleths and merge population data to merged dataframe

--- src/utils.py
+++ src/utils.py
@@ -9,3 +9,25 @@ from urllib.request import urlopen
 import json
 import plotly.express as px
 import plotly.graph_objects as go
+
+def makeChoropleth(df,valueColumn,title,colorRange = [0,0]):
+    nonNanDF = df[df[valueColumn] != np.nan]
+    
+    if colorRange[1] == 0:
+        colorRange[1] = max(nonNanDF[valueColumn])
+
+    fig = px.choropleth(
+        nonNanDF,
+        locations = 'Country/Territory',
+        locationmode = 'country names',
+        color = valueColumn,
+        range_color = colorRange,
+        color_continuous_scale='RdYlGn'
+    )
+    
+    fig.layout.coloraxis.colorbar.title = ""
+
+    fig.update_layout(title_text=title)
+    imgTitle = title.replace(" ","")
+    fig.write_image(f'../images/{imgTitle}.png')
+    return fig
